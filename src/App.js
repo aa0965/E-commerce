5b31829d3405c26aa137eccf31b13e283017bcc9
aa0965 <anshupratapsingh32391@gmail.com>
2021-02-10 19:54:00 +0530

    'collection'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Switch ,Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
+
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
